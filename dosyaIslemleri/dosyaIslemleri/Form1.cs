e066f99e4aba61357fdf69bcf2bd926460f34fe9
Raziye AYDIN <raziye.aydin@bil.omu.edu.tr>
2014-07-15 10:08:44 +0300

    createDirectory

---
+++ dosyaIslemleri/dosyaIslemleri/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace dosyaIslemleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String name;
+        String yol;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            name = textBox1.Text;
+            yol = "C:\\Users\\bynMhnds\\Desktop\\"+name;
+            Directory.CreateDirectory(yol);
+
+            MessageBox.Show("klasörünüz oluşturuldu");
+        }
+
+    }
+}
